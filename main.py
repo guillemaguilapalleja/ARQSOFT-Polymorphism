1b411d8e932546a675ed073a8c4d931ab16eca77
guillemaguilapalleja <guillemaguila@10-192-163-237client.eduroam.upc.edu>
2023-10-16 09:55:25 +0200

    Extract main logic into a function

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from source.SortingAlgorithms.ComparatorDirInsertSorter import ComparatorDirInse
 from source.models import Building
 from random import randint
 
-if __name__ == '__main__':
+
+def main() -> None:
     building1: Building = Building(volume_cube_meters=randint(10, 20), height_meters=randint(1, 5))
     building2: Building = Building(volume_cube_meters=randint(1, 8), height_meters=randint(6, 9))
     building3: Building = Building(volume_cube_meters=randint(100, 200), height_meters=randint(1, 3))
@@ -21,3 +22,7 @@ if __name__ == '__main__':
 
     print("List after sorting with Bubble sorter:", buildings_list)
     print("List after sorting with Dir Insert sorter:", copy_buildings_list)
+
+
+if __name__ == '__main__':
+    main()
